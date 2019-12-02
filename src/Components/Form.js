3ff55f6aa83a2921fa8ad47d97a6849267fa6adf
IncomReact <romain.baly@gmail.com>
2019-12-02 16:58:14 +0100

    maj todo

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -10,37 +10,27 @@ class Form extends Component {
       text: '',
       items: [],
     };
-    // console.log('this.state.items ==>',this.state.items)
   }
 
-  
-  
   onChange = (event) => {
     this.setState({text: event.target.value});
   }
 
-  
-  
   onSubmit = (event) => {
     event.preventDefault();
     if(this.state.text !== '') {
       this.setState({
         text: '',
-<<<<<<< HEAD
-        items: [...this.state.items, this.state.text], 
-      })
-      // this.props.update({dataForm : this.state.items});
-    }    
-=======
         items: [...this.state.items, this.state.text]
-	  }, () => {
-		//   this.props.update({dataForm : this.state.items});
-		// ICI dans le cas du callBack, c'ets bien plus intéressant que le CWU car evite les boucles infinits. 
-	});
+    }
+  //   , () => {
+	// 	  this.props.update({dataForm : this.state.items});
+	// 	// ICI dans le cas du callBack, c'ets bien plus intéressant que le CWU car evite les boucles infinits. 
+  // }
+  );
       
     }
     
->>>>>>> 8c6b8d2f2fb67bbf6638ddc41319b4e3b9bd721e
   }
   componentDidUpdate() {
 	if(this.state.items !== this.props.liststate) {
@@ -48,35 +38,16 @@ class Form extends Component {
 	}
   }
 
-  componentDidUpdate() {
-    if (this.state.items.length !== 0 ) {
-      this.props.update({dataForm : this.state.items});
-    } 
-   }
-   
-
-  
-
     render() { 
-<<<<<<< HEAD
-      // console.log('this.state.items', this.state.items)
-      
-=======
-	  console.log('this.state.items', this.state.items);
+	  // console.log('this.state.items', this.state.items);
     
->>>>>>> 8c6b8d2f2fb67bbf6638ddc41319b4e3b9bd721e
         return (
             <React.Fragment>
                 <form className="flex space-between" onSubmit={this.onSubmit}>
                     <input className="add-task" placeholder="Ajouter une tâche" value={this.state.text} onChange={this.onChange} />
                     <button>ADD</button>
-                </form>
-                
-                {/* <List data={this.state.items}/>  */}
-                
-            </React.Fragment>
-
-            
+                </form>    
+            </React.Fragment>   
         )
     }
 }
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -11,40 +11,36 @@ class Todo extends Component {
         this.state = {
           globalItems : []
         };
+        console.log('this.state.globalItems ===>',this.state.globalItems)
       }
 
       
+      
      onUpdate = (dataForm) => {
-<<<<<<< HEAD
-        console.log('dataForm ==>', dataForm)
         
-         this.setState({   
-            globalItems : [...this.state.globalItems, dataForm]
-        })
-=======
-		console.log('dataForm ==>', dataForm)
+        console.log('dataForm ==>', dataForm)
+        // this.setState({   
+        //     globalItems : [...this.state.globalItems, dataForm]
+        // })
 		if(this.state.globalItems.indexOf(dataForm) === false) {
 			this.setState({   
 				globalItems : [...this.state.globalItems, dataForm]
 			});
-		}
->>>>>>> 8c6b8d2f2fb67bbf6638ddc41319b4e3b9bd721e
+        }
+        
      } 
 
     
 
     render() {
-        // const globalItems = this.state.globalItems
-        // console.log('globalItems ===>',globalItems)
+        
+        let globalItems = this.state.globalItems
+        console.log('globalItems ===>', globalItems)
         
         return (
             <React.Fragment>
-<<<<<<< HEAD
-                <Form update = {this.onUpdate}/>
-                <List data={this.state.globalItems}/>
-=======
                 <Form update={this.onUpdate} liststate={this.state.globalItems} />
->>>>>>> 8c6b8d2f2fb67bbf6638ddc41319b4e3b9bd721e
+                <List data={this.state.globalItems}/> 
             </React.Fragment>
         )
     }
